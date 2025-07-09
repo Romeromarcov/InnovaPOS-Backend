277c1c68b518cda36bf315e3b1cd3acefc17961b
Marco Romero <romeromarcovinicio@gmail.com>
2025-07-09 10:16:23 -0400

    Cambios al 9/7/25 10:15 am

--- innovapos_backend/urls.py
+++ innovapos_backend/urls.py
@@ -2,6 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from rest_framework import routers
 from inventario import views # Importa las vistas de tu app original
 
@@ -48,5 +52,6 @@ router.register(r'cuotas-prestamo', views.CuotasPrestamoViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
